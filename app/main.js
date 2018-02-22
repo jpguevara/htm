3c7bde0ac83d14342580584b738445023879e4d9
JP Guevara <chorew@gmail.com>
2018-02-21 21:46:54 -0600

    added webpack

---
+++ app/main.js
@@ -0,0 +1,16 @@
+// import code from other files like this
+var otherModule = require('./other-module');
+
+console.log('write your entry code here');
+
+// call other module functions
+otherModule.someFunction();
+
+
+const x = 0;
+let y = [1,2,3];
+let z = [...y]; // use ES6 spread operator 
+
+// use "template literals" aka backtick strings 
+console.log(`z= ${z}`);
+
---
+++ config/webpack.config.base.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var rootPath = process.cwd();
+
+module.exports = {
+  context: rootPath + '/app',
+  entry: {
+    app: './main.js',
+    // vendor: ['angular','@uirouter/angularjs']
+
+  },
+  output: {
+    path: path.resolve(rootPath, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: "babel-loader", // or "babel" because webpack adds the '-loader' automatically
+      query: {
+        presets: ['env']
+      },
+      // "include" is commonly used to match the directories
+      include: [
+        path.resolve(rootPath, "app/"),
+        // path.resolve(rootPath, "app/test")
+      ]
+    }]
+  },
+  resolve: {
+    modules: [
+      path.join(rootPath, "node_modules")
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title:'Webpack Html Starter'})
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   filename: "vendor.bundle.js"
+    // })
+  ],
+  stats: {
+    colors: true
+  }
+};
